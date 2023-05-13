7921de8fa823b101023dc83a5e5096b6c4f0e4bb
AndrzejSotnikov <sotnikow.dev@gmail.com>
2023-05-13 11:27:30 +0200

    add server function, HomeRoute.test refactoring

--- src/routes/HomeRoute.test.js
+++ src/routes/HomeRoute.test.js
@@ -1,35 +1,22 @@
 import { render, screen } from '@testing-library/react';
-import { setupServer } from 'msw/node';
-import { rest } from 'msw';
 import { MemoryRouter } from 'react-router-dom';
 import HomeRoute from './HomeRoute';
+import { createServer } from '../test/server'
 
-const handlers = [
-    rest.get('/api/repositories', (req, res, ctx) => {
+createServer([{
+    path: '/api/repositories',
+    res: (req) => {
         const language = req.url.searchParams.get('q').split('language:')[1];
-        console.log(language)
-        return res(
-            ctx.json({
-                items: [
-                    { id: 1, full_name: `${language}_one` },
-                    { id: 2, full_name: `${language}_two` },
-                ],
-            })
-        );
-    }),
-];
 
-const server = setupServer(...handlers);
+        return {
+            items: [
+                { id: 1, full_name: `${language}_one` },
+                { id: 2, full_name: `${language}_two` },
+            ]
+        }
+    }
+}])
 
-beforeAll(() => {
-    server.listen();
-});
-afterEach(() => {
-    server.resetHandlers();
-});
-afterAll(() => {
-    server.close();
-});
 
 test('renders two links for each language', async () => {
     render(
