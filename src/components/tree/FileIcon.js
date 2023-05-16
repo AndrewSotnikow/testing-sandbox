4dae8186e962fe06dcaa613c84dc23e0cc8d6068
AndrzejSotnikov <sotnikow.dev@gmail.com>
2023-05-17 00:46:10 +0200

    add AuthTest

--- src/components/tree/FileIcon.js
+++ src/components/tree/FileIcon.js
@@ -1,10 +1,10 @@
-import "@exuanbo/file-icons-js/dist/css/file-icons.css";
-import icons from "@exuanbo/file-icons-js/dist/js/file-icons";
-import { useEffect, useState } from "react";
-import classNames from "classnames";
+import '@exuanbo/file-icons-js/dist/css/file-icons.css';
+import icons from '@exuanbo/file-icons-js/dist/js/file-icons';
+import { useEffect, useState } from 'react';
+import classNames from 'classnames';
 
 function FileIcon({ name, className }) {
-  const [klass, setKlass] = useState("");
+  const [klass, setKlass] = useState('');
 
   useEffect(() => {
     icons
